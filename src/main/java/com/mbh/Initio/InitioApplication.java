b75debfc8352ec20a79cfa89074a6e6d9f1d4506
Mohammed Hussain <mohammadhussain9924@gmail.com>
2026-09-18 09:21:09 -0400

    Initial commit

---
+++ src/main/java/com/mbh/Initio/InitioApplication.java
@@ -0,0 +1,13 @@
+package com.mbh.Initio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbh/Initio/InitioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbh.Initio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
